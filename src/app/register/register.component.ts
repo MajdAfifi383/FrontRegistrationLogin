2e95d97dc925d574c17046f6069ca1ef30845bca
unknown <MajdAfifi383>
2024-02-16 10:42:34 +0100

    first commit

---
+++ src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  employeename: string ="";
+  email: string ="";
+  password: string ="";
+  constructor(private http: HttpClient )
+  {
+  }
+  save()
+  {
+  
+    let bodyData = {
+      "employeename" : this.employeename,
+      "email" : this.email,
+      "password" : this.password
+    };
+    this.http.post("http://localhost:8089/api/v1/employee/save",bodyData,{responseType: 'text'}).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Employee Registered Successfully");
+    });
+  }
+}
